9bdb772fb658d85d7dd5d5445de562a81de881bd
otwald <hinterlanddev@googlemail.com>
2025-07-16 17:39:55 +0200

    work from 1607

--- map/testmap.gd
+++ map/testmap.gd
@@ -3,8 +3,8 @@ var unno = load("res://map/unno.tscn")
 var stone = load("res://map/stone.tscn")
 var baum = load("res://map/baum.tscn")
 var copper = load("res://map/copperstatik.tscn") 
-var randrom_world_gen = [unno,stone,baum,copper]
-
+var baum_2d = load("res://map/baum/baum_2d.tscn")
+var randrom_world_gen = [unno,stone,baum,copper,baum_2d]
 
 
 func _ready() -> void:
@@ -13,8 +13,9 @@ func _ready() -> void:
 	
 func init_map():
 	for n in 100:
-		var new_object = randrom_world_gen[randi_range(0,3)]
-		var position = Vector3(randi_range(0,20),0,randi_range(0,20)) 
+		var length = randrom_world_gen.size() -1
+		var new_object = randrom_world_gen[randi_range(0,length)]
+		var position = Vector3(randi_range(-62,62),0.3,randi_range(-62,62)) 
 		var newplayer = new_object.instantiate()
 		add_child(newplayer)
 		newplayer.global_position =position
